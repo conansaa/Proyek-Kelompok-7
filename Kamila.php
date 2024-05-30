62fdb6194a85d2157638dabbca64513cbadcf7a4
Kamilafp <kamilafp25@gmail.com>
2024-05-30 19:25:01 +0700

    Create Kamila.php

---
+++ Kamila.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>H1D022035</title>
+    </head>
+    <body>
+        <h1>BIODATA DIRI:</h1>
+        <img src="C:\Users\kamil\OneDrive\Gambar\Rol Kamera\H1D022035.jpg" title="H1D022035" height="300">
+        <b>
+        <p>Nama: Kamila Fajar Pertiwi</p>
+        <p> NIM: H1D022035</p>
+        <p>Kota: Sragen</p>
+        </b>
+    </body>
+</html>
