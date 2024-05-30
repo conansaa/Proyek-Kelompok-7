a57eea4aa423245967c08175884d43e25d2c43af
Melyanarizky <150778053+Melyanarizky@users.noreply.github.com>
2024-05-30 21:06:42 +0700

    Create melyana.php

---
+++ melyana.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>H1D022013</title>
+    </head>
+    <body>
+        <h1>BIODATA DIRI:</h1>
+        <img src="C:\Users\kamil\OneDrive\Gambar\Rol Kamera\H1D022035.jpg" title="H1D022035" height="300">
+        <b>
+        <p>Nama: Melyana Rizky Ramadhani</p>
+        <p> NIM: H1D022013</p>
+        </b>
+    </body>
+</html>
